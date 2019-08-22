5930f419614c15ca9f70cd829ff9e527fb8b8156
Feng Yifei <fengyfei@gmail.com>
2019-08-22 17:46:05 +0800

    lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'root.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   Provider.debugCheckInvalidValueType = null;
 
   runApp(Application());
