f06b6a7ca4d0824d32119c2dcacabbd0c345e100
Feng Yifei <fengyfei@gmail.com>
2019-08-22 21:50:54 +0800

    result logic

--- lib/pages/quizzes/result.dart
+++ lib/pages/quizzes/result.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
@@ -11,10 +12,56 @@ class ResultWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final store = Provider.of<QuizStore>(context);
 
-    return Container(
-      child: Observer(
-        builder: (_) => Text('Errors ${store.errors}'),
-      ),
+    return Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 80,
+          ),
+          height: 320,
+          child: SvgPicture.asset(
+            Resources.svgResultBackground,
+            //color: Colors.black12,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        SafeArea(
+          child: Container(
+            margin: EdgeInsets.only(
+              top: 420,
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  child: Observer(
+                    builder: (_) {
+                      return Text('学习完成，有 ${store.errors} 个错误',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.lightBlue,
+                          fontSize: 24,
+                        ),
+                      );
+                    }
+                  ),
+                ),
+                Container(
+                  height: 120,
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '返回首页'
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
